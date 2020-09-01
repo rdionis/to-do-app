be6a459dda4dd17d989d7e606b75d9a1f2dd927f
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-09-01 09:49:12 +0200

    Updates

---
+++ precache-manifest.86458eea053197b5e0220248c9ca6650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cff7094cb37c471b1ca07bed95bf45",
+    "url": "/rdionis/to-do-app/index.html"
+  },
+  {
+    "revision": "0d7a8fee80203fad0130",
+    "url": "/rdionis/to-do-app/static/css/main.b1657efd.chunk.css"
+  },
+  {
+    "revision": "ca1ac7e3fe3f563cfcd4",
+    "url": "/rdionis/to-do-app/static/js/2.3ec21336.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rdionis/to-do-app/static/js/2.3ec21336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7a8fee80203fad0130",
+    "url": "/rdionis/to-do-app/static/js/main.e8666b76.chunk.js"
+  },
+  {
+    "revision": "0f970d91dbac8b6f2c89",
+    "url": "/rdionis/to-do-app/static/js/runtime-main.9b9124b6.js"
+  }
+]);
